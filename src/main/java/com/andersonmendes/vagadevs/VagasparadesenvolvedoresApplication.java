244d6e7ccd64f46ff925d213f073adcf626ef0e5
Anderson Mendes <mendes.ams@gmail.com>
2023-02-13 23:02:11 -0300

    Criação do projeto

---
+++ src/main/java/com/andersonmendes/vagadevs/VagasparadesenvolvedoresApplication.java
@@ -0,0 +1,13 @@
+package com.andersonmendes.vagadevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagasparadesenvolvedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagasparadesenvolvedoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andersonmendes/vagadevs/VagasparadesenvolvedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersonmendes.vagadevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VagasparadesenvolvedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
