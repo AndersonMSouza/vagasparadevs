46c586f7b339803bd7b225e35b89a0c7741b1791
Anderson Mendes <mendes.ams@gmail.com>
2023-07-24 23:14:15 -0300

    Criando exceptionhandles tratarHttpMediaTypeNotSupportedException.

--- src/main/java/com/andersonmendes/vagadevs/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/andersonmendes/vagadevs/api/exceptionhandler/ApiExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -38,6 +39,15 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	public ResponseEntity<?> tratarNegocioException(NegocioException ex, WebRequest request) {
 		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), 
 				HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+	public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem("O tipo de mídia não é aceito!").build();
+		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+				.body(problema);
 	}
 	
 	
